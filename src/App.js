1043b5a0d416b19cbfe8c460c3a28260efa01f0a
milkyboy94 <53465675+milkyboy94@users.noreply.github.com>
2022-10-19 19:24:08 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+      });
+  }, []);
+  const recoin = coins.slice(0, 20);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <strong>"loading..."</strong>
+      ) : (
+        <div>
+          <h1 className="coin_title">Coin Trackers({recoin.length})</h1>
+
+          <div className="below_title">
+            <ul className="api_table">
+              <div className="top_label">
+                <div className="label1">Coin</div>
+                <div className="label2">Symbol</div>
+                <div className="label3">Price</div>
+                <div className="label4">Volume</div>
+                <div className="label5">From ATH</div>
+              </div>
+              {recoin.map((coin) => (
+                <li className="coin_list">
+                  <div className="coin_table">
+                    <div className="coin_name">{coin.name}</div>{" "}
+                    <div className="coin_symbol">({coin.symbol})</div>
+                    <div className="coin_price">
+                      {coin.quotes.USD.price.toFixed(1)} USD
+                    </div>
+                    <div className="coin_volume">
+                      {coin.quotes.USD.volume_24h.toFixed(1)}
+                    </div>
+                    <div className="coin_ath">
+                      {coin.quotes.USD.percent_from_price_ath}
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+            <div>Transfer USD to Coin</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
